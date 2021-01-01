47a89938bab5b3a368a49baa862bacb727a37b01
Patrick Goley <patrick.goley@gmail.com>
2020-12-31 23:58:17 -0600

    Initial Commit

---
+++ CatDogClassifier/CatDogClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatDogClassifierApp.swift
+//  CatDogClassifier
+//
+//  Created by Patrick Goley on 12/31/20.
+//
+
+import SwiftUI
+
+@main
+struct CatDogClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
